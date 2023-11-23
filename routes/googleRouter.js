1a2f55784e36f1f7721293231d361752216ce3c7
omaima sayed <116375938+OmaimaSayedmohamed2022@users.noreply.github.com>
2023-11-23 16:09:33 +0200

    Update googleRouter.js

--- routes/googleRouter.js
+++ routes/googleRouter.js
@@ -14,6 +14,7 @@ passport.use(new GoogleStrategy(
     scope: ['profile', 'email'],
   },
   function(accessToken, refreshToken, profile, cb) {
+     console.log('accessToken:', accessToken);
     userProfile=profile;
     return cb(null, userProfile);
 }
