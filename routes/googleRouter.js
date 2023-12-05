04a5e5da6031b7705b29b127fc43d2a335c2d9e4
omaima sayed <116375938+OmaimaSayedmohamed2022@users.noreply.github.com>
2023-12-05 12:59:56 +0200

    Update googleRouter.js

--- routes/googleRouter.js
+++ routes/googleRouter.js
@@ -8,26 +8,29 @@ const verifyToken = require('../config/verifyToken')
 
 passport.use(new GoogleStrategy(
   {
-    clientID: process.env.clientID,
-    clientSecret: process.env.clientSecret,
-    callbackURL: 'https://projectscopey.onrender.com/auth/google/callback',
+    clientID: process.env.client_ID,
+    clientSecret: process.env.client_Secret,
+    callbackURL: process.env.Gmail_Callback_URL,
     scope: ['profile', 'email'],
   },
   function(accessToken, refreshToken, profile, cb) {
-     console.log('accessToken:', accessToken);
     userProfile=profile;
     return cb(null, userProfile);
 }
 ));
 
 router.get('/', passport.authenticate('google', { scope: ['profile', 'email'] }));
-router.get('/callback', passport.authenticate('google', { failureRedirect: 'https://scopey.onrender.com/auth/google/error' }), googleController.google_SignIn);
+router.get('/callback', passport.authenticate('google', { failureRedirect: 'https://projectscopey.onrender.com/auth/google/error' }), googleController.google_SignIn);
 router.get('/success', googleController.google_SignIn);
 router.get('/error', (req, res) => res.status(500).json({ status: 0, error: 'Error logging in via Google.' }));
-
+ 
 router.get('/logout',verifyToken, googleController.google_Logout);
 //get all google users 
 router.get('/data',verifyToken, googleController.get_gmail_users);
+
+
+
+module.exports = router;
 
 
 
